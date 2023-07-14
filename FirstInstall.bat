a209b19f7c3befecf342565cdcc053bb8cb890df
Gaethanos__ <48406088+GaetanoDEV@users.noreply.github.com>
2023-07-14 13:19:05 +0200

    Aggiunti moduli

--- FirstInstall.bat
+++ FirstInstall.bat
@@ -4,3 +4,6 @@ cd etc
 python-3.11.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 echo Installazione delle dipendenze...
 pip install mysql-connector-python
+pip install requests
+pip install bs4
+pip install screeninfo
